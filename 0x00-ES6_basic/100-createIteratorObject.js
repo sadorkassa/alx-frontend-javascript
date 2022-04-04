193360371d9af3e64c34befc7d010fe3743fd5b8
Sador Kassa <sadorkassa21@gmail.com>
2022-04-04 14:12:04 +0000

    0x00-ES6_basic

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,7 +1,12 @@
 export default function createIteratorObject(report) {
-  const emp = [];
-  for (const k of Object.keys(report.allEmployees)) {
-    emp.push(...report.allEmployees[k]);
+  const workers = [];
+  /* eslint-disable no-unused-vars */
+  for (const [department, employees] of Object.entries(report.allEmployees)) {
+    for (const employe of employees) {
+      workers.push(employe);
+    }
   }
-  return emp;
+  /* eslint-enable no-unused-vars */
+
+  return workers;
 }
